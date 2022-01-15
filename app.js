f024949e87811604710c5777e8ec1772e49896ed
unknown <golovanov911@gmail.com>
2022-01-15 15:53:29 +0300

    ves

---
+++ app.js
@@ -0,0 +1,9 @@
+
+alert("Seychas mi uznaem est' li u vas problemi s lishnim veson.Najmite ok");
+let x=prompt('V stroke napishite vash ves v kg');
+let y=prompt("Teper' napishite svoi rost v metrah");
+
+
+let z=x/(y**2);
+
+console.log(alert('Imeete li vi lishniy ves?'+(z>=25)));
